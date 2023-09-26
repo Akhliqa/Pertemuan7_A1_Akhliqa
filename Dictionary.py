58c22672862aef4b0fe7a08ca072e668caaa5ed9
Akhliqa <143387256+Akhliqa@users.noreply.github.com>
2023-09-26 20:59:31 +0700

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,54 @@
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
+print()
+student = {
+    "nama" : "faqih",
+    "umur" : 26,
+    "tiggi" : 177.6
+}
+print(student)
+student["hobi"]="coding"
+print(student)
+print()
+student = {
+    "nama" : "faqih",
+    "umur" : 26,
+    "tiggi" : 177.6
+}
+print(student)
+student.update({"hobi" : "coding"})
+print(student)
+print()
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8 
+dict['School'] = "DPS School"
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
+print()
+student = {
+    "nama" : "faqih",
+    "umur" : 26,
+    "tiggi" : 177.6
+}
+print(student)
+student["nama"] = "darmawan"
+print(student)
+print()
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+del dict['Name'] # hapus entri dengan key 'Name'
+dict.clear()
+del dict
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
+print()
+student = {
+    "nama" : "faqih",
+    "umur" : 26,
+    "tiggi" : 177.6
+}
+print(student)
+student.pop("umur")
+print(student)
+
+
